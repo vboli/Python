5a81015a82a06ed658d79988c1a72a2c7cc3783f
vboli <vishwaboli@gmail.comm>
2024-10-15 11:21:54 +0530

    Initial commit

---
+++ 5.Modules/package/maths.py
@@ -0,0 +1,10 @@
+# package/maths.py
+def addition(a, b):
+    return a + b
+
+def substraction(a,b):
+    return a - b
+
+def multiplication(a,b):
+    return a * b
+    
---
+++ 5.Modules/test.py
@@ -0,0 +1,7 @@
+from package.maths import *
+from package.subpackage.div import division
+
+print(addition(2,3))
+print(substraction(6,2))
+print(multiplication(4,3))
+print(division(6,2))
